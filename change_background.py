f28372f7f387e5bf21e2105fb90b496919351062
Carlos Gomez <carlosandres0741@gmail.com>
2022-01-28 20:52:21 -0500

    Add: script to change background

---
+++ change_background.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from time import sleep
+from os import getcwd, scandir
+from subprocess import check_output
+from sys import argv
+
+path = argv[1] if len(argv) > 1 else getcwd()
+
+def main(path = getcwd()):
+    while True:
+        for img in scandir(path):
+            command = f"feh --bg-fill {path}/{img.name}"
+            if img.name.endswith(".jpg") or img.name.endswith(".png"):
+                check_output(command, shell=True)
+                sleep(60)
+
+main(path)
